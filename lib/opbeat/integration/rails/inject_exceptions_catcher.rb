b23a123cd94dc20c8011e48c116df763c7b7d9c2
Julien Levesy <jlevesy@gmail.com>
2016-08-02 10:41:03 +0200

    fix the opbeat-gem for rails 5

--- lib/opbeat/integration/rails/inject_exceptions_catcher.rb
+++ lib/opbeat/integration/rails/inject_exceptions_catcher.rb
@@ -6,15 +6,15 @@ module Opbeat
           cls.send(:alias_method_chain, :render_exception, :opbeat)
         end
 
-        def render_exception_with_opbeat(env, exception)
+        def render_exception_with_opbeat(request, exception)
           begin
-            Opbeat.report(exception, rack_env: env) if Opbeat.started?
+            Opbeat.report(exception, rack_env: request.env) if Opbeat.started?
           rescue
             ::Rails::logger.error "** [Opbeat] Error capturing or sending exception #{$!}"
             ::Rails::logger.debug $!.backtrace.join("\n")
           end
 
-          render_exception_without_opbeat(env, exception)
+          render_exception_without_opbeat(request, exception)
         end
       end
     end
